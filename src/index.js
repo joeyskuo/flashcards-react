d79de9107afc8b121f75474bc882bb22bf0d6312
Unknown <joeyskuo@gmail.com>
2017-08-20 21:20:48 -0700

    added reducers and actions

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
-import { BrowserRouter, Switch, Route } from 'react-router-dom';
+import {BrowserRouter, Switch, Route} from 'react-router-dom';
+import Stack from './components/Stack';
 
 ReactDOM.render(
     <BrowserRouter>
         <Switch>
             <Route exact path='/' component={App}/>
+            <Route path='/stack' component={Stack}/>
         </Switch>
     </BrowserRouter>,
-        document.getElementById('root')
+    document.getElementById('root')
 );
