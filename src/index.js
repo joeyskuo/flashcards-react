0125a455f32453e60fee4fbddb2428f20b3fcd46
Unknown <joeyskuo@gmail.com>
2017-08-24 23:34:18 -0700

    added flashcard creation route

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import App from './components/App';
 import rootReducer from './reducers';
 import {BrowserRouter, Switch, Route} from 'react-router-dom';
 import Stack from './components/Stack';
+import StackForm from './components/StackForm';
 import { setStack } from './actions';
 import { Provider } from 'react-redux';
 import './index.css';
@@ -20,6 +21,7 @@ ReactDOM.render(
             <Switch>
                 <Route exact path='/' component={App}/>
                 <Route path='/stack' component={Stack}/>
+                <Route path='/stack_form' component={StackForm}/>
             </Switch>
         </BrowserRouter>
     </Provider>,
